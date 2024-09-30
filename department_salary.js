d450b3532850326a8b053310a2fad399fdd1994d
AdityaSharma42 <adityasharma3@usf.com>
2024-09-29 23:28:07 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -28,7 +28,7 @@ const company = {
             ]
         },
         {
-            departmentName: 'IT,
+            departmentName: 'IT',
             employees: [
                 {
                     name: 'Jade',
@@ -42,7 +42,7 @@ const company = {
                     ]
                 },
                 {
-                    name: 'Grace',
+                    name: 'Vin',
                     salary: 95000,
                     subordinates: []
                 }
@@ -50,3 +50,26 @@ const company = {
         }
     ]
 };
+function calculateDepartmentSalary (employees){
+    return employees.reduce((totalSalary, employee)=>{
+        totalSalary += employee.salary;
+    
+        totalSalary += employee.subordinates.reduce((subTotal,subordinate)=>{
+            return subTotal + calculateDepartmentSalary([subordinate]);
+        },0);
+    
+
+    return totalSalary;
+    },0);
+}
+
+const itDepartment= company.departments.find(dep=>dep.departmentName=== 'IT');
+const accountingDepartment = company.departments.find(dep=>dep.departmentName==='Accounting');
+
+const totalSalaryIT= calculateDepartmentSalary(itDepartment.employees);
+const totalSalaryAccounting= calculateDepartmentSalary(accountingDepartment.employees);
+
+console.log (`The total salary of the IT department is: $${totalSalaryIT}`);
+console.log (`The total salary of the Accounting department is: $${totalSalaryAccounting}`);
+
+
